ee5b624b98ae9c223b79eb8b429ceb733cc64961
fa020 <abhishek@aargusinfotec.com>
2025-06-06 14:20:09 +0530

    in the request path variable is woking

--- src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
+++ src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.one.demo_one.dto.EmployeeDTO;
@@ -17,6 +18,10 @@ public class EmployeeController {
     @GetMapping(path = "/employe/detail")
     public EmployeeDTO getEmployeesDetails(){
         return new EmployeeDTO(12L,"a",LocalDate.of(2000, 02, 20),true);
+    }
+    @GetMapping(path = "/employe/detail/{id}")
+    public EmployeeDTO getEmployeesDetailsById(@PathVariable("id") Long empID){
+        return new EmployeeDTO(empID,"a",LocalDate.of(2000, 02, 20),true);
     }
 
 }
