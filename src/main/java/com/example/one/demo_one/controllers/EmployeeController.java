3930e2fef8c1bbd6934f207c9dff0c842454d442
fa020 <abhishek@aargusinfotec.com>
2025-06-06 14:11:41 +0530

    route /employe/detail is showing the data but - isActive field is changed to the active just the field name change and it will be solved by - adding annotation JsonProperty

--- src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
+++ src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.example.one.demo_one.controllers;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.one.demo_one.dto.EmployeeDTO;
 
 @RestController
 public class EmployeeController {
     @GetMapping(path = "/e")
     public String getEmployees(){
         return "aaa";
+    }
+    @GetMapping(path = "/employe/detail")
+    public EmployeeDTO getEmployeesDetails(){
+        return new EmployeeDTO(12L,"a",LocalDate.of(2000, 02, 20),true);
     }
 
 }
