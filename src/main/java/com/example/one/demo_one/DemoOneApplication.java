3637ae94d9738a40c42bfd3eaf5fd99a50a100fa
fa020 <abhishek@aargusinfotec.com>
2025-06-06 11:49:39 +0530

    prod DB object is created with new keyword

--- src/main/java/com/example/one/demo_one/DemoOneApplication.java
+++ src/main/java/com/example/one/demo_one/DemoOneApplication.java
@@ -1,13 +1,24 @@
 package com.example.one.demo_one;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoOneApplication {
+public class DemoOneApplication implements CommandLineRunner {
+	ProdDB prodDB;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOneApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		// throw new UnsupportedOperationException("Unimplemented method 'run'");
+		prodDB = new ProdDB();
+		System.out.println(prodDB.getData());
+
 	}
 
 }
---
+++ src/main/java/com/example/one/demo_one/ProdDB.java
@@ -0,0 +1,8 @@
+package com.example.one.demo_one;
+
+public class ProdDB {
+    public String getData(){
+        return "pro suctione  ";
+    }
+
+}
