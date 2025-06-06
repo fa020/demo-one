72f2417160c045176a3b2b9fb9be6e8eb131e3ed
fa020 <abhishek@aargusinfotec.com>
2025-06-06 17:08:50 +0530

    i added the data from postgerss pg admin and fetch it from postman and it is working fine - some things i have done in pg admin for the id and this i have done in ak2 db

--- src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
+++ src/main/java/com/example/one/demo_one/controllers/EmployeeController.java
@@ -8,20 +8,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.one.demo_one.dto.EmployeeDTO;
+import com.example.one.demo_one.services.EmployeeService;
 
 @RestController
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
     @GetMapping(path = "/e")
     public String getEmployees(){
         return "aaa";
     }
     @GetMapping(path = "/employe/detail")
     public EmployeeDTO getEmployeesDetails(){
+        System.out.println("here");
         return new EmployeeDTO(12L,"a",LocalDate.of(2000, 02, 20),true);
     }
+    // @GetMapping(path = "/employe/detail/{id}")
+    // public EmployeeDTO getEmployeesDetailsById(@PathVariable("id") Long empID){
+    //     return new EmployeeDTO(empID,"a",LocalDate.of(2000, 02, 20),true);
+    // }
     @GetMapping(path = "/employe/detail/{id}")
     public EmployeeDTO getEmployeesDetailsById(@PathVariable("id") Long empID){
-        return new EmployeeDTO(empID,"a",LocalDate.of(2000, 02, 20),true);
+        System.out.println("here 2");
+        System.out.println(empID);
+        return employeeService.getEmpById(empID);
     }
 
 }
---
+++ src/main/java/com/example/one/demo_one/services/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.example.one.demo_one.services;
+
+import org.springframework.stereotype.Service;
+
+import com.example.one.demo_one.dto.EmployeeDTO;
+import com.example.one.demo_one.entities.EmployeeEntity;
+import com.example.one.demo_one.repositeries.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    final EmployeeRepository employeeRepository;
+
+
+    public EmployeeService(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    public EmployeeDTO getEmpById(Long id){
+        EmployeeEntity employeeEntity = employeeRepository.getById(id);
+        return new EmployeeDTO(employeeEntity.getId(),employeeEntity.getName(),employeeEntity.getDateOfJoinging(),employeeEntity.isActive());
+    }
+
+
+}
