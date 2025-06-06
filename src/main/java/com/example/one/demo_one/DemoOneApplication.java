14ca7aef152a627461a7da409c77cc5009bf88b5
fa020 <abhishek@aargusinfotec.com>
2025-06-06 12:37:46 +0530

    now afer adding ConditionalOnProperty and in application properties the variable name it is now loosely copuled

--- src/main/java/com/example/one/demo_one/DemoOneApplication.java
+++ src/main/java/com/example/one/demo_one/DemoOneApplication.java
@@ -1,11 +1,13 @@
 package com.example.one.demo_one;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoOneApplication implements CommandLineRunner {
+	@Autowired
 	DB db;
 
 	public static void main(String[] args) {
--- src/main/java/com/example/one/demo_one/configuration/AppConfig.java
+++ src/main/java/com/example/one/demo_one/configuration/AppConfig.java
@@ -1,15 +1,23 @@
 package com.example.one.demo_one.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.one.demo_one.DB;
+import com.example.one.demo_one.DevDB;
 import com.example.one.demo_one.ProdDB;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public DB getDBBean() {
+    @ConditionalOnProperty(name = "project.mode", havingValue = "pro")
+    public DB getProdDBBean() {
         return new ProdDB();
+    }
+    @Bean
+    @ConditionalOnProperty(name = "project.mode", havingValue = "dev")
+    public DB getDevDBBean() {
+        return new DevDB();
     }
 }
